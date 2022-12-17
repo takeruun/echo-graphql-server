154a1dc871337b0ea309d1ba523d7ea1a4e6ac71
takeru.hashimoto <hstake25@gmail.com>
2022-12-17 17:47:41 +0900

    usecases

---
+++ usecases/auth.usecase.go
@@ -0,0 +1,128 @@
+package usecases
+
+import (
+	"app/database"
+	"app/entity"
+	"app/graph/model"
+	"app/services"
+	"time"
+
+	"context"
+	"errors"
+)
+
+const (
+	ACCESS_TOKEN_KEY = "_echo_graphql_access_token"
+)
+
+type AuthUsecase interface {
+	SignIn(ctx context.Context, signInParams *model.SignInInput) error
+	SignUp(ctx context.Context, signInParams *model.SignUpInput) error
+	Show(ctx context.Context) (user *entity.User, err error)
+	Delete(ctx context.Context) error
+}
+
+type authUsecase struct {
+	userRepo      database.UserRepository
+	cookieService services.CookieService
+	cyptoService  services.CyptoService
+	jwtService    services.JwtService
+}
+
+func NewAuthUsecase(userRepo database.UserRepository, cookieService services.CookieService, cyptoService services.CyptoService, jwtService services.JwtService) AuthUsecase {
+	return &authUsecase{
+		userRepo:      userRepo,
+		cookieService: cookieService,
+		cyptoService:  cyptoService,
+		jwtService:    jwtService,
+	}
+}
+
+func (uu *authUsecase) SignIn(ctx context.Context, signInParams *model.SignInInput) error {
+	loginUser, err := uu.userRepo.FindByEmail(signInParams.Email)
+	if err != nil {
+		return err
+	}
+
+	if !uu.cyptoService.ComparePasswords(loginUser.HashPassword, []byte(signInParams.Password)) {
+		return errors.New("Authentication Failure")
+	}
+
+	accessToken, err := uu.jwtService.GenerateToken(uint(loginUser.ID), time.Now())
+	if err != nil {
+		return err
+	}
+
+	err = uu.cookieService.SetCookie(ctx, ACCESS_TOKEN_KEY, accessToken)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (uu *authUsecase) SignUp(ctx context.Context, signInParams *model.SignUpInput) error {
+	hashPwd, err := uu.cyptoService.HashAndSalt([]byte(signInParams.Password))
+	if err != nil {
+		return err
+	}
+
+	u := entity.User{Name: signInParams.Name, Email: signInParams.Email, HashPassword: hashPwd}
+	loginUser, err := uu.userRepo.Create(&u)
+	if err != nil {
+		return err
+	}
+
+	accessToken, err := uu.jwtService.GenerateToken(uint(loginUser.ID), time.Now())
+	if err != nil {
+		return err
+	}
+
+	err = uu.cookieService.SetCookie(ctx, ACCESS_TOKEN_KEY, accessToken)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (uu *authUsecase) Show(ctx context.Context) (user *entity.User, err error) {
+	accessToken, err := uu.cookieService.GetCookieValue(ctx, ACCESS_TOKEN_KEY)
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := uu.jwtService.ParseToken(accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err = uu.userRepo.Find(uint64(auth.Uid))
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (uu *authUsecase) Delete(ctx context.Context) error {
+	accessToken, err := uu.cookieService.GetCookieValue(ctx, ACCESS_TOKEN_KEY)
+	if err != nil {
+		return err
+	}
+
+	auth, err := uu.jwtService.ParseToken(accessToken)
+	if err != nil {
+		return err
+	}
+
+	if err := uu.userRepo.Delete(uint64(auth.Uid)); err != nil {
+		return nil
+	}
+
+	if err := uu.cookieService.DeleteCookie(ctx, ACCESS_TOKEN_KEY); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ usecases/todo.usecase.go
@@ -0,0 +1,109 @@
+package usecases
+
+import (
+	"app/database"
+	"app/entity"
+	"app/graph/model"
+	"app/services"
+	"context"
+	"errors"
+)
+
+type TodoUsecase interface {
+	FindAll(ctx context.Context) (todos []*entity.Todo, err error)
+	Create(ctx context.Context, createParams *model.CreateTodo) (todos *entity.Todo, err error)
+	Show(ctx context.Context, todoId int) (todo *entity.Todo, err error)
+	Edit(todoId int, updateParams *model.UpdateTodo) (todo *entity.Todo, err error)
+	Delete(ctx context.Context, todoId int) error
+}
+
+type todoUsecase struct {
+	todoRepo      database.TodoRepository
+	cookieService services.CookieService
+	jwtService    services.JwtService
+}
+
+func NewTodoUsecase(todoRepo database.TodoRepository, cookieService services.CookieService, jwtService services.JwtService) TodoUsecase {
+	return &todoUsecase{
+		todoRepo:      todoRepo,
+		cookieService: cookieService,
+		jwtService:    jwtService,
+	}
+}
+
+func (tu *todoUsecase) FindAll(ctx context.Context) (todos []*entity.Todo, err error) {
+	accessToken, err := tu.cookieService.GetCookieValue(ctx, ACCESS_TOKEN_KEY)
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := tu.jwtService.ParseToken(accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	todos, err = tu.todoRepo.FindAll(uint64(auth.Uid))
+
+	return
+}
+
+func (tu *todoUsecase) Create(ctx context.Context, createParams *model.CreateTodo) (todo *entity.Todo, err error) {
+	accessToken, err := tu.cookieService.GetCookieValue(ctx, ACCESS_TOKEN_KEY)
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := tu.jwtService.ParseToken(accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	t := entity.Todo{Title: createParams.Title, Description: createParams.Description, UserId: uint64(auth.Uid)}
+	todo, err = tu.todoRepo.Create(&t)
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (tu *todoUsecase) Show(ctx context.Context, todoId int) (todo *entity.Todo, err error) {
+	accessToken, err := tu.cookieService.GetCookieValue(ctx, ACCESS_TOKEN_KEY)
+	if err != nil {
+		return nil, err
+	}
+
+	todo, err = tu.todoRepo.Find(todoId)
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := tu.jwtService.ParseToken(accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	if uint64(auth.Uid) != todo.UserId {
+		return nil, errors.New("no your todo")
+	}
+
+	return
+}
+
+func (tu *todoUsecase) Edit(todoId int, updateParams *model.UpdateTodo) (todo *entity.Todo, err error) {
+	t := entity.Todo{ID: uint64(todoId), Description: updateParams.Description}
+	todo, err = tu.todoRepo.Update(&t)
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func (tu *todoUsecase) Delete(ctx context.Context, todoId int) error {
+	if err := tu.todoRepo.Delete(&entity.Todo{ID: uint64(todoId)}); err != nil {
+		return err
+	}
+
+	return nil
+}
